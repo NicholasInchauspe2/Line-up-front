d7946024368bf0f5b8dc6eba21ce15488d9dc361
MatiasOrlando <matiassorlando@gmail.com>
2023-04-25 21:26:39 +0200

    testing render

--- client/src/pages/reserva.js
+++ client/src/pages/reserva.js
@@ -4,7 +4,6 @@ import { getSession } from "next-auth/react";
 export async function getServerSideProps(context) {
   const sessionToken = context.req.cookies["next-auth.session-token"];
   const session = await getSession(context);
-
   if (!sessionToken) {
     return {
       redirect: {
@@ -15,15 +14,17 @@ export async function getServerSideProps(context) {
   } else {
     if (session.user) {
       const res = await fetch(
-        `https://matias-lineup.onrender.com/api/user/validate/token?token=${session.user}`,
-        {},
-        { withCredentials: true, credentials: "include" }
+        // `https://matias-lineup.onrender.com/api/user/validate/token?token=${session.user}`,
+        `http://localhost:3001/api/user/validate/token?token=${session.user}`,
+        {}
+        // { withCredentials: true, credentials: "include" }
       );
       const data = await res.json();
       const response = await fetch(
-        "https://matias-lineup.onrender.com/api/appointments/branches",
-        {},
-        { withCredentials: true, credentials: "include" }
+        // "https://matias-lineup.onrender.com/api/appointments/branches",
+        "http://localhost:3001/api/appointments/branches",
+        {}
+        // { withCredentials: true, credentials: "include" }
       );
       const branchData = await response.json();
 
